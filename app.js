c281c0f8442cf094373a1ed498c3caa6a44d288e
jsgitter <oscar.chiaradia@gmail.com>
2021-10-18 21:57:01 -0300

    cuelgo el procfile

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require( "express" );
+
+const path    = require( "path" );
+
+const app = express();
+
+const PORT    = 3014;
+
+app.use( express.static( path.join( __dirname, `./public` ) ) );
+
+app.listen( PORT, () => { console.log( `Express arriba en port ${PORT}` ) } );
+
+//app.get( "/", (req,res) => { res.send( "Express arriba " ) } );
+
+app.get( "/", (req,res) => { res.sendFile( path.resolve( __dirname, "./views/index.html" ) ) } );
+app.get( "/registro", (req,res) => { res.sendFile( path.resolve( __dirname, "./views/registro.html" ) ) } );
+app.get( "/login", (req,res) => { res.sendFile( path.resolve( __dirname, "./views/login.html" ) ) } );
+
+
+
